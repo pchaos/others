2469217e61bcb89f9d425735fd1d6a7a3f3fc5e2
pchaos <drifthua@gmail.com>
2017-11-07 01:31:24 -0600

    python 首次安装执行脚本

---
+++ tips/python.sh
@@ -0,0 +1,10 @@
+# python 首次安装执行脚本
+
+conda update conda
+conda update --all
+
+conda install numexpr numpy pandas lxml matplotlib beautifulsoup4 scrapy
+conda install sqlalchemy sqlite
+conda install bcolz
+
+pip install fake-useragent TA-Lib
