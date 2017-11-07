5b043ac638548c8fe576d1b0d5c54255abc3c556
pchaos <drifthua@gmail.com>
2017-11-06 22:28:19 -0600

    Update install_rqalpha.sh

--- linux/install_rqalpha.sh
+++ linux/install_rqalpha.sh
@@ -1,7 +1,13 @@
 
+#设置matplotlib的backend（没有图形化界面的情况下）
+echo "backend: Agg" > ~/.config/matplotlib/matplotlibrc
 
 pip install -U pip setuptools cython
 
 pip install bcolz
 
 pip install rqalpha
+
+rqalpha version
+
+rqalpha update_bundle
