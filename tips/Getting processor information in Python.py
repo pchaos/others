efaeb3ab956b0e3f3b90cd8de70bb84b7d82a74d
pchaos <drifthua@gmail.com>
2017-11-07 01:19:20 -0600

    Create Getting processor information in Python.py

---
+++ tips/Getting processor information in Python.py
@@ -0,0 +1,19 @@
+# Getting processor information in Python
+
+
+import os, platform, subprocess, re
+
+def get_processor_name():
+    if platform.system() == "Windows":
+        return platform.processor()
+    elif platform.system() == "Darwin":
+        os.environ['PATH'] = os.environ['PATH'] + os.pathsep + '/usr/sbin'
+        command ="sysctl -n machdep.cpu.brand_string"
+        return subprocess.check_output(command).strip()
+    elif platform.system() == "Linux":
+        command = "cat /proc/cpuinfo"
+        all_info = subprocess.check_output(command, shell=True).strip()
+        for line in all_info.decode("utf-8").split("\n"):
+            if "model name" in line:
+                return re.sub( ".*model name.*:", "", line,1)
+    return ""
