c3e6f17f993b7013ca41927fdf78fbe8396e42de
pchaos <drifthua@gmail.com>
2017-11-20 03:41:13 -0600

    Create 修改 centos yum 为国内源.sh

---
+++ linux/修改 centos yum 为国内源.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# http://mirrors.aliyun.com/help/centos
+# 修改 centos yum 为国内源
+	
+mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
+wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+
+yum clean all && yum makecache
