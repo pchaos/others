d8df4cab2cf8446169b37429585f6fe3189f7160
pchaos <drifthua@gmail.com>
2017-11-22 23:15:33 +0800

    Update 修改 centos yum 为国内源.sh

--- linux/修改 centos yum 为国内源.sh
+++ linux/修改 centos yum 为国内源.sh
@@ -6,3 +6,6 @@ mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
 wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
 
 yum clean all && rm -rf /var/cache/yum, && yum makecache
+
+# CentOS 7添加官方的 Remi 源
+yum localinstall --nogpgcheck http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
