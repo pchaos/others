2f47720b832b708da8715026b0c0aa223c698b72
pchaos <drifthua@gmail.com>
2017-12-08 23:00:30 +0800

    Update ubuntu for lede k3.sh

--- linux/ubuntu for lede k3.sh
+++ linux/ubuntu for lede k3.sh
@@ -4,12 +4,12 @@ sudo apt-get install -y ssh
 
 #sudo with nopassword
 sudo visudo
-3 yg ALL=(ALLi:ALL) NOPASSWD:ALL
+# yg ALL=(ALL) NOPASSWD:ALL
 
 # 源码主页见 https://github.com/coolsnowwolf/lede
 sudo apt-get update --fix-missing
 sudo apt-get install -y apt-utils
-mkdir ~/lede
+# mkdir ~/lede
 sudo apt-get install -y build-essential git wget sudo vim git bzip2
 sudo apt-get install -y asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev
 sudo apt-get install -y genisoimage perl
