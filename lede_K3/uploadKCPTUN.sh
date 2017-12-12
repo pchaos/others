06eb1de5436c6d387f76230f0894e10965d870f4
pchaos <drifthua@gmail.com>
2017-12-12 02:43:12 -0600

    Create uploadKCPTUN.sh

---
+++ lede_K3/uploadKCPTUN.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /tmp/upload && chmod a+x client_linux_arm5 
+
+mv /tmp/upload/client_linux_arm5 /usr/share/kcptun/
+mv /tmp/upload/client*.json /usr/share/kcptun/
+cd /usr/share/kcptun/ && mv client_linux_arm5 kcptun
