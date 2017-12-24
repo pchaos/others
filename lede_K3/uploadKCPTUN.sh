ab9f0713c508faba14218bdeeb93d27d85207f0c
pchaos <drifthua@gmail.com>
2017-12-23 19:13:02 -0600

    Update uploadKCPTUN.sh

--- lede_K3/uploadKCPTUN.sh
+++ lede_K3/uploadKCPTUN.sh
@@ -7,3 +7,7 @@ mv /tmp/upload/client*.json /usr/share/kcptun/
 cd /usr/share/kcptun/ && mv client_linux_arm5 kcptun
 
 mv /tmp/upload/client_linux_arm5 /var/kcptun_client
+
+
+/etc/adhosts
+/var/kcptun_client
