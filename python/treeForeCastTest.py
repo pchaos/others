a1e54c1cbf20cbfcff4da44ca1f72fceafb4522e
pchaosgit <drifthua@gmail.com>
2017-12-25 22:48:14 +0800

    剔除新股

--- python/treeForeCastTest.py
+++ python/treeForeCastTest.py
@@ -22,7 +22,7 @@ eii=0
 for st in slist:
     if st[:3] not in {'sh2', 'sh5', 'sh9',  'sz1', 'sz2'}:
         df = ts.get_k_data(code = st, start = '2017-01-01') # 获取股票数据
-        if len(df) > 0:
+        if len(df) > 100: # 剔除新股
             # 股票有数据
             e = pd.DataFrame()
             e['idx'] = df.index # 用索引号保证顺序X轴
