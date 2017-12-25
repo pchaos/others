c3414baa78d09a63ff2d0a62b8554b75f4c98cb8
pchaosgit <drifthua@gmail.com>
2017-12-25 22:39:18 +0800

    优化算法

--- python/treeForeCastTest.py
+++ python/treeForeCastTest.py
@@ -12,12 +12,13 @@ output_file = 'BaiduStockInfo.txt'
 slist=[]
 SHSZStockCode.getStockList(slist, stock_list_url)
 
-rightCode=[]
-falseCode=[]
-kickpoint=[]
+uptrend=[] # 上升趋势
+downtrend=[] # 下降趋势
+kickpoint=[] # 转折点
 ri=0
 fii=0
 kii=0
+eii=0
 for st in slist:
     if st[:3] not in {'sh2', 'sh5', 'sh9',  'sz1', 'sz2'}:
         df = ts.get_k_data(code = st, start = '2017-01-01') # 获取股票数据
@@ -27,18 +28,23 @@ for st in slist:
             e['idx'] = df.index # 用索引号保证顺序X轴
             e['close'] = df['close'] # 用收盘价作为分类标准Y轴, 以Y轴高低划分X成段，并分段拟合
             arr = np.array(e)
-            tree = treeForeCast.createTree(np.mat(arr), 100, 10)
-            yHat = treeForeCast.createForeCast(tree, arr[:,0])
-            d=yHat[-3:]
-            if d[1]<= d[2]:
-                rightCode.append(st)
-                ri+=1
-                if d[1]< d[0]:
-                    # 转折点
-                    kickpoint.append(st)
-                    kii+=1
-            else:
-                falseCode.append(st)
-                fii+=1
-            print(ri, fii, kii, st)
+            try:
+                tree = treeForeCast.createTree(np.mat(arr), 100, 10)
+                yHat = treeForeCast.createForeCast(tree, arr[:,0])
+                d=yHat[-5:]
+                if d[3]<= d[4]:
+                    uptrend.append(st)
+                    ri+=1
+                    md=np.min(d)
+                    if (md < d[0]) and (md < d[4]):
+                        # 转折点
+                        kickpoint.append(st)
+                        kii+=1
+                else:
+                    downtrend.append(st)
+                    fii+=1
+            except Exception as ex:
+                eii+=1
+                print( ex)
+            print(ri, fii, kii, eii, st)
             #sleep(0.5)
