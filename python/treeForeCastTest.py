f11d03301d80a2173949afb71c850ca2a3826509
pchaosgit <drifthua@gmail.com>
2017-12-25 21:35:39 +0800

    update treeForeCastTest.py

--- python/treeForeCastTest.py
+++ python/treeForeCastTest.py
@@ -1,5 +1,10 @@
 from time import sleep
 import SHSZStockCode
+import treeForeCast
+import tushare as ts
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 # 获取股票代码
 stock_list_url = 'http://quote.eastmoney.com/stocklist.html'
@@ -7,6 +12,10 @@ output_file = 'BaiduStockInfo.txt'
 slist=[]
 SHSZStockCode.getStockList(slist, stock_list_url)
 
+rightCode=[]
+falseCode=[]
+ri=0
+fii=0
 for st in slist:
     if st[:3] not in {'sh2', 'sh5', 'sh9',  'sz1', 'sz2'}:
         df = ts.get_k_data(code = st, start = '2017-01-01') # 获取股票数据
@@ -16,10 +25,15 @@ for st in slist:
             e['idx'] = df.index # 用索引号保证顺序X轴
             e['close'] = df['close'] # 用收盘价作为分类标准Y轴, 以Y轴高低划分X成段，并分段拟合
             arr = np.array(e)
-            tree = createTree(np.mat(arr), 100, 10)
-            yHat = createForeCast(tree, arr[:,0])
+            tree = treeForeCast.createTree(np.mat(arr), 100, 10)
+            yHat = treeForeCast.createForeCast(tree, arr[:,0])
             d=yHat[-2:]
             if d[0]<= d[1]:
-                print(1)
-            print(len(df), st)
+                rightCode.append(st)
+                ri+=1
+                print(ri)
+            else:
+                falseCode.append(st)
+                fii+=1
+            print(ri, fii, st)
             #sleep(0.5)
