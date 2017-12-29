0c894bde7ca67ffb03b64558f84a25616a23d393
yg <yg@centospython-i.lan>
2017-12-29 16:14:36 +0800

    多图叠加

--- python/treeForeCast.py
+++ python/treeForeCast.py
@@ -7,6 +7,55 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
+#日期格式YYYYMMDD转为YYYY-MM-DD  
+def formatDate(Date, formatType='YYYYMMDD'):  
+    formatType = formatType.replace('YYYY', Date[0:4])  
+    formatType = formatType.replace('MM', Date[4:6])  
+    formatType = formatType.replace('DD', Date[-2:])  
+    return formatType  
+
+def get_data(code=None, start='', end='',
+                  ktype='D', autype='qfq', 
+                  index=False,
+                  retry_count=3):
+	"""
+	获取k线数据
+	---------
+	Parameters:
+	code:string
+		  股票代码 e.g. 600848
+	start:string
+		  开始日期 format：YYYY-MM-DD 为空时取上市首日
+	end:string
+		  结束日期 format：YYYY-MM-DD 为空时取最近一个交易日
+	autype:string
+		  复权类型，qfq-前复权 hfq-后复权 None-不复权，默认为qfq
+	ktype：string
+		  数据类型，D=日k线 W=周 M=月 5=5分钟 15=15分钟 30=30分钟 60=60分钟，默认为D
+	retry_count : int, 默认 3
+		 如遇网络等问题重复执行的次数 
+	return
+	-------
+	DataFrame
+	  date 交易日期 (index)
+	  open 开盘价
+	  high  最高价
+	  close 收盘价
+	  low 最低价
+	  volume 成交量
+	  amount 成交额
+	  turnoverratio 换手率
+	  code 股票代码
+	"""
+	dh=ts.get_k_data(code,start=start,end=end) #不复权  
+	dg=ts.get_k_data(code,start=start,end=end,autype='hfq')     #后复权  
+	del dg['code']  
+	del dg['date']  
+	del dg['volume']  
+	dg.rename(columns={'open':'fopen', 'close':'fclose', 'high':'fhigh', 'low':'flow'}, inplace = True)      
+	mergeColumn=pd.concat([dh,dg],axis=1)
+	return mergeColumn
+
 # 用feature把dataSet按value分成两个子集
 def binSplitDataSet(dataSet, feature, value):
     mat0 = dataSet[np.nonzero(dataSet[:,feature] > value)[0],:]
@@ -97,15 +146,16 @@ def createForeCast(tree, testData):
     return yHat
 
 # 绘图
-def draw(dataSet, tree, title, loc='left'):
-    plt.figure(figsize=[20,11]) # 改变画布大小
+def draw(dataSet, tree, title, loc='left', color ='red',drawEnd=True):
+    #plt.figure(figsize=[20,11]) # 改变画布大小
     plt.title(title, loc=loc);
     plt.xlabel('Time')
     plt.ylabel('收盘价')
     plt.scatter(dataSet[:,0], dataSet[:,1], s=5) # 在图中以点画收盘价
     yHat = createForeCast(tree, dataSet[:,0])
-    plt.plot(dataSet[:,0], yHat, linewidth=2.0, color='red')
-    plt.show()
+    plt.plot(dataSet[:,0], yHat, linewidth=2.0, color=color)
+    if drawEnd:
+        plt.show()
 
 # 生成回归树, dataSet是数据, rate是误差下降, dur是叶节点的最小样本数
 def createTree(dataSet, rate, dur):
@@ -122,13 +172,18 @@ def createTree(dataSet, rate, dur):
     return retTree
 
 if __name__ == '__main__':
-    stCode='600051'
+    sbdf=ts.get_stock_basics()  
+    Code=sbdf.index
+
+    stCode='601155'
     idx=0
-    stDate='2017-01-01'
+    stDate=sbdf.loc[stCode]['timeToMarket']                   #上市日期YYYYMMDD  
+    stDate=formatDate(str(stDate),'YYYY-MM-DD')            #改一下格式 
+    stDate='2015-01-01'
     start = time.time()
 #    df = ts.get_k_data(code = '002230', start = '2017-01-01') # 科大讯飞今年的股票数据
     # df = ts.get_k_data(code = '600026', start = '2016-01-01', ktype='30') # 股票数据
-    df = ts.get_k_data(code = stCode, start = stDate, index=(idx!=0))
+    df = get_data(code = stCode, start = stDate, index=(idx!=0))
     cons=None
     #cons=ts.get_apis()
     #df = ts.bar( '603766', conn=cons, freq='60min',adj='qfq', start_date='2016-01-01')
@@ -139,11 +194,18 @@ if __name__ == '__main__':
         df.index = pd.RangeIndex(len(df.index))
     e = pd.DataFrame()
     e['idx'] = df.index # 用索引号保证顺序X轴
+    e['close'] = df['fclose'] # 用收盘价作为分类标准Y轴, 以Y轴高低划分X成段，并分段拟合
+    arr = np.array(e)
+    tree = createTree(np.mat(arr), 100, 10)
+    end = time.time()
+    plt.figure(figsize=[20,11]) # 改变画布大小
+    draw(arr, tree, stCode + ' start: ' + stDate + " TotalTime:" + str(end-start) + " " + str((end-start)/len(df)), drawEnd= False)
     e['close'] = df['close'] # 用收盘价作为分类标准Y轴, 以Y轴高低划分X成段，并分段拟合
     arr = np.array(e)
     tree = createTree(np.mat(arr), 100, 10)
     end = time.time()
-    draw(arr, tree, stCode + ' start: ' + stDate + " TotalTime:" + str(end-start) + " " + str((end-start)/len(df)))
+    draw(arr, tree, stCode + ' start: ' + stDate + " TotalTime:" + str(end-start) + " " + str((end-start)/len(df)), color = 'blue')
+
     if cons:
         ts.close_apis(cons)
 
