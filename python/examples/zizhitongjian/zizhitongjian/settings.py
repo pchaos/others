c903cdaff9540bedb34b8b0193199b2baebb577c
pchaosgit <drifthua@gmail.com>
2018-03-20 14:38:44 +0800

    完成爬取资治通鉴

--- python/examples/zizhitongjian/zizhitongjian/settings.py
+++ python/examples/zizhitongjian/zizhitongjian/settings.py
@@ -17,10 +17,11 @@ NEWSPIDER_MODULE = 'zizhitongjian.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'zizhitongjian (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36'
 
 # Obey robots.txt rules
-# ROBOTSTXT_OBEY = True
-ROBOTSTXT_OBEY = False
+ROBOTSTXT_OBEY = True
+# ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -28,9 +29,9 @@ ROBOTSTXT_OBEY = False
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 2
 # The download delay setting will honor only one of:
-CONCURRENT_REQUESTS_PER_DOMAIN = 16
+# CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
--- python/examples/zizhitongjian/zizhitongjian/spiders/zztj.py
+++ python/examples/zizhitongjian/zizhitongjian/spiders/zztj.py
@@ -8,7 +8,7 @@ from zizhitongjian.items import ZizhitongjianItem, ZizhitongjianContentItem
 class ZztjSpider(scrapy.Spider):
     name = 'zztj'
 
-    allowed_domains = ['https://www.sbkk88.com/mingzhu/gudaicn/zizhitongjian/*']
+    allowed_domains = ['www.sbkk88.com']
     start_urls = ['https://www.sbkk88.com/mingzhu/gudaicn/zizhitongjian//']
     base_urls = 'https://www.sbkk88.com/mingzhu/gudaicn/zizhitongjian/'
 
@@ -45,13 +45,13 @@ class ZztjSpider(scrapy.Spider):
         # 获取每章内容
         subtitile = response.xpath('//*[@id="f_title1"]/h1/text()').extract_first()
         lContent = response.xpath('//*[@id="f_article"]')
-        sContent = ""
-        for i in lContent.css('p::text').extract():
-            sContent += i
-            sContent += '\r\n'
-
-        sContent = sContent.replace("\n", "").replace("\t", "")
-        linkText = sContent
+        # sContent = ""
+        # for i in lContent.css('p::text').extract():
+        #     sContent += i
+        #     sContent += '\r\n'
+        # if (len(sContent) < 5):
+        #     log.info("SCRAP NONE ,RESPONSE.TEXT: {0}".format(lContent.extract_first()))
+        sContent = lContent.extract_first().replace("<u>一</u>", "")
         item = ZizhitongjianItem()
         item['title'] = response.meta['title']
         item['link'] = response.meta['link']
