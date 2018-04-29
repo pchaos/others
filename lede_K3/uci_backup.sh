aaf43301a04eaf915ee5cc270ac87300851b60cf
pchaos <drifthua@gmail.com>
2018-04-29 12:24:37 +0800

    Update uci_backup.sh

--- lede_K3/uci_backup.sh
+++ lede_K3/uci_backup.sh
@@ -4,14 +4,40 @@ uci show shadowsocksr
 uci export shadowsocksr  > /tmp/shadowsocksr.ini
 uci export wireless  > /tmp/wireless.ini
 uci export sfe  > /tmp/sfe.ini
-network.wanhe
-network.lan
-luci.themes
-kcptun
-firewall.shadowsocksr
-dhcp.@host[0]
+uci export network.wanhe  > /tmp/network.wanhe.ini
+uci export network.lan  > /tmp/network.lan.ini
+uci export luci.themes  > /tmp/luci.themes.ini
+uci export kcptun  > /tmp/kcptun.ini
+uci export firewall.shadowsocksr  > /tmp/firewall.shadowsocksr.ini
+uci export dhcp.@host[0]  > /tmp/dhcp.@host[0].ini
+uci export dhcp.@host[0]  > /tmp/dhcp.@host[0].ini
+uci export dhcp.@host[0]  > /tmp/dhcp.@host[0].ini
+uci export dhcp.@host[0]  > /tmp/dhcp.@host[0].ini
+uci export dhcp.@host[0]  > /tmp/dhcp.@host[0].ini
+uci export dhcp.@host[0]  > /tmp/dhcp.@host[0].ini
+uci export dhcp.@host[0]  > /tmp/dhcp.@host[0].ini
+uci export dhcp.@host[0]  > /tmp/dhcp.@host[0].ini
+uci export dhcp.@host[0]  > /tmp/dhcp.@host[0].ini
+uci export dhcp.@host[0]  > /tmp/dhcp.@host[0].ini
+
+uci export dhcp.@domain[0]  > /tmp/dhcp.@domain[0].ini
+uci export dhcp.@domain[0]  > /tmp/dhcp.@domain[0].ini
+uci export dhcp.@domain[0]  > /tmp/dhcp.@domain[0].ini
+uci export dhcp.@domain[0]  > /tmp/dhcp.@domain[0].ini
+uci export dhcp.@domain[0]  > /tmp/dhcp.@domain[0].ini
+uci export dhcp.@domain[0]  > /tmp/dhcp.@domain[0].ini
+uci export dhcp.@domain[0]  > /tmp/dhcp.@domain[0].ini
+
+uci export dhcp.lan  > /tmp/dhcp.lan.ini
+uci export ddns  > /tmp/ddns.ini
+
+
+uci export sfe  > /tmp/sfe.ini
+uci export sfe  > /tmp/sfe.ini
+uci export sfe  > /tmp/sfe.ini
+
 dhcp.@host[9]
-dhcp.@domain[0]
+
 dhcp.@domain[6]
 dhcp.lan
-ddns
+
