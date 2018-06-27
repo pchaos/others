03e4044977ba133826b26558ad97e98ef1a9a041
pchaos <drifthua@gmail.com>
2018-06-27 15:21:44 +0800

    Update aliyun_centos7.sh

--- linux/aliyun_centos7.sh
+++ linux/aliyun_centos7.sh
@@ -3,14 +3,12 @@ yum update -y
 yum install -y screen upx git bzip2
 reboot
 
-useradd yg
-passwd yg
+useradd yg && passwd yg
 
 screen
 su yg
 cd 
-mkdir download
-cd download
+mkdir download && cd download
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod a+x ./Miniconda3-latest-Linux-x86_64.sh 
 ./Miniconda3-latest-Linux-x86_64.sh 
@@ -20,6 +18,6 @@ conda update --all
 conda create --name stock
 
 source activate stock
-pip install bs4 requests lxml cython pandas
+pip install bs4 requests lxml cython pandas django
 pip install mkl
 pip install tushare quantaxis
