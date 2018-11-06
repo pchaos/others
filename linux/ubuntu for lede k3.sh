458519668bf88e68d292733a7d54a00f70d79597
pchaos <14939791+pchaos@users.noreply.github.com>
2018-11-06 14:14:13 +0800

    Update ubuntu for lede k3.sh

--- linux/ubuntu for lede k3.sh
+++ linux/ubuntu for lede k3.sh
@@ -12,7 +12,7 @@ sudo apt-get install -y apt-utils
 # mkdir ~/lede
 sudo apt-get install -y build-essential git wget sudo vim git bzip2
 sudo apt-get install -y asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev
-sudo apt-get install -y genisoimage perl
+sudo apt-get install -y genisoimage perl automake
 sudo apt-get install --reinstall binutils
 
 export LEDEK3=$HOME/lede
