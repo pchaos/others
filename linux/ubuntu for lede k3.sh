12213a68e4aa4825f2e447b20297931ef787c9c0
pchaos <14939791+pchaos@users.noreply.github.com>
2018-11-28 19:00:43 +0800

    Update ubuntu for lede k3.sh

--- linux/ubuntu for lede k3.sh
+++ linux/ubuntu for lede k3.sh
@@ -38,6 +38,6 @@ make -j1 V=s
 
 默认登陆IP 192.168.1.1, 密码 password
 
-# 2018 11 28 shadowsocksr不kill v2ray
-sed -i 's/killall -q -9 v2ray/#killall -q -9 v2ray/g' shadowsocksr
+# 2018 11 28 shadowsocksr不kill v2ray,路由器上执行
+sed -i 's/killall -q -9 v2ray/#killall -q -9 v2ray/g' /etc/init.d/shadowsocksr
 
