9cb88752ba26b95dacb1af8b07d07508edf381fc
pchaos <14939791+pchaos@users.noreply.github.com>
2019-06-27 10:24:51 +0800

    Create imgConvert.py

---
+++ python/imgConvert.py
@@ -0,0 +1,12 @@
+# can all the webp file and convert them to jpg
+from PIL import Image
+import os
+
+for (dirname, dirs, files) in os.walk("."):
+     for filename in files:
+         if filename.endswith('.webp'):
+             print('found: ' + os.path.splitext(filename)[0])
+             print('converting to: ' + os.path.splitext(filename)[0] + '.jpg')
+             im = Image.open(filename).convert("RGB")
+             im.save(os.path.splitext(filename)[0] + '.jpg', "jpeg")
+             print('done converti')
