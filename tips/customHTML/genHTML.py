93ccd153c1862f288aa85491998b82f1c2609757
pchaos <drifthua@gmail.com>
2019-08-28 23:30:41 +0800

    update custom html with jinja2

--- tips/customHTML/genHTML.py
+++ tips/customHTML/genHTML.py
@@ -2,7 +2,7 @@
 """
 -------------------------------------------------
    File Name：     genHTML
-   Description :
+   Description : 从配置ini文件中生成
    Author :       pchaos
    date：          2019/8/28
 -------------------------------------------------
@@ -13,7 +13,9 @@
 __author__ = 'pchaos'
 
 from bs4 import BeautifulSoup
+import jinja2
 
+dlist = []
 flist = ["hacker.html"]
 for fn in flist:
 	with open('{}.ini'.format(fn), "r") as f:
@@ -22,18 +24,28 @@ for fn in flist:
 	alist = fnini.split("\n")
 	for a in alist:
 		# print(a.split("||"))
-		ddcit = {}
-		dlist = []
+		ddict = {}
+
 		try:
-			d = {"text": "", "href": ""}
+			item = {"caption": "", "href": ""}
 			t, h = a.split("||")
-			d["text"] = t
-			d["href"] = h
+			item["caption"] = t
+			item["href"] = h
 		except Exception as e:
 			pass
-		dlist.append(d)
+		ddict.update(dlist)
+		dlist.append(item)
 
 		# print(dlist.keys(), dlist.values())
-		for d in dlist:
-			print(d)
-			print(d["text"], d["href"])
+		# for item in dlist:
+		# 	print(item)
+		# 	print(item["caption"], item["href"])
+
+templateLoader = jinja2.FileSystemLoader(searchpath="./")
+templateEnv = jinja2.Environment(loader=templateLoader)
+TEMPLATE_FILE = "template.html"
+template = templateEnv.get_template(TEMPLATE_FILE)
+outputText = template.render(title="HACKER", sites=dlist)  # this is where to put args to the template renderer
+
+with open('/tmp/{}'.format(fn), "w+") as f:
+	f.write(outputText)
