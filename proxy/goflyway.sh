4abaabd532bfb583c86334a4758f832b3862ea5b
pchaos <drifthua@gmail.com>
2019-09-24 19:41:29 +0800

    goflyway

--- proxy/goflyway.sh
+++ proxy/goflyway.sh
@@ -2,8 +2,9 @@
 
 if [ ! $key ];then key="doubi233"; fi
 
-proxypass="http://support.cloudflare.com/hc/en-us"
+#proxypass="http://support.cloudflare.com/hc/en-us"
 proxypass="http://mirror.centos.org/centos/7/os/x86_64/"
 
+killall goflyeay
 #nohup ./goflyway -k="$key" -l=":8080" -proxy-pass="http://kernel.ubuntu.com/~kernel-ppa/mainline/" > /tmp/goflyway.log 2>&1 &
 nohup ./goflyway -k="$key" -l=":8080" -proxy-pass="$proxypass" > /tmp/goflyway.log 2>&1 &
