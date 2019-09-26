a5690cbb0f7cb3c8cc7f2748dc8333d0670f2c72
pchaos <drifthua@gmail.com>
2019-09-26 19:53:07 +0800

    exchange text

--- proxy/abc.sh
+++ proxy/abc.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
+wget https://raw.githubusercontent.com/JeffJiangHub/goflyway/master/Makefile
 #mkdir goflywaybuild &&
 export GOPATH=$PWD/goflywaybuild
 #go get github.com/mitchellh/gox
@@ -9,4 +10,8 @@ cd $GOPATH/bin
 GOOS=linux
 GOARCH=amd64
 #./gox -os "windows linux" -arch amd64 $GOPATH/src/github.com/coyove/goflyway/cmd/goflyway
-go build $GOPATH/src/github.com/coyove/goflyway/cmd/goflyway/main.go
+./gox -os "linux" -arch amd64 $GOPATH/src/github.com/coyove/goflyway/cmd/goflyway
+./gox -os "linux" -arch arm $GOPATH/src/github.com/coyove/goflyway/cmd/goflyway
+./gox -os "linux" -arch mips $GOPATH/src/github.com/coyove/goflyway/cmd/goflyway
+#go build $GOPATH/src/github.com/coyove/goflyway/cmd/goflyway/main.go
+ll
