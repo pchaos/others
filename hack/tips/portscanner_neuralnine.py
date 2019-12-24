2a1d060fc1f83ec2dfb2ab814814780f84a8e868
pchaos <drifthua@gmail.com>
2019-12-24 15:02:34 +0800

    portscanner

---
+++ hack/tips/portscanner_neuralnine.py
@@ -0,0 +1,63 @@
+# Multithreaded Port Scanner
+# by NeuralNine Copyright (c) 2019
+from queue import Queue
+import socket
+import threading
+
+target = "127.0.0.1"
+queue = Queue()
+open_ports = []
+
+
+def portscan(port):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((target, port))
+        return True
+    except:
+        return False
+
+def get_ports(mode):
+    if mode == 1:
+        for port in range(1, 1024):
+            queue.put(port)
+    elif mode == 2:
+        for port in range(1, 49152):
+            queue.put(port)
+    elif mode == 3:
+        ports = [20, 21, 22, 23, 25, 53, 80, 110, 443]
+        for port in ports:
+            queue.put(port)
+    elif mode == 4:
+        ports = input("Enter your ports (seperate by blank):")
+        ports = ports.split()
+        ports = list(map(int, ports))
+        for port in ports:
+            queue.put(port)
+
+def worker():
+    while not queue.empty():
+        port = queue.get()
+        if portscan(port):
+            print("Port {} is open!".format(port))
+            open_ports.append(port)
+
+def run_scanner(threads, mode):
+
+    get_ports(mode)
+
+    thread_list = []
+
+    for t in range(threads):
+        thread = threading.Thread(target=worker)
+        thread_list.append(thread)
+
+    for thread in thread_list:
+        thread.start()
+
+    for thread in thread_list:
+        thread.join()
+
+    print("Open ports are:", open_ports)
+
+run_scanner(100, 1)
