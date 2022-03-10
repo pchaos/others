96d1e761443c7f72a470988da301f4fc6a7569e1
pchaos <drifthua@gmail.com>
2022-03-10 21:39:41 +0800

    update racknerd restart

--- python/selenium/racknerd/racknerd.py
+++ python/selenium/racknerd/racknerd.py
@@ -1,186 +1,132 @@
-# -*- coding: utf-8 -*- #
+# -*- coding: utf-8 -*-
+"""登录racknerd cloud，重启vps
+"""
 import os
-#  import json
 import time
-#  import random
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
-import pickle
 from dotenv import load_dotenv
-
 # explicitly providing path to '.env'
 from pathlib import Path  # Python 3.6+ only
+from seleniumbase import BaseCase
 
-env_path = Path('.') / '.env'
-load_dotenv(dotenv_path=env_path, verbose=True)
+class RACKNERDTest(BaseCase):
+    """ RACKNERD Cloud Test
+    """
+    @classmethod
+    def setUpClass(cls):
 
-email = os.getenv("EMAIL")
-password = os.getenv("PASSWORD")
-searchcounts = int(os.getenv("SEARCHCOUNTS"))  # 查询次数
-isProxy = int(os.getenv("PROXY"))  # 是否用proxy
-isRestart = int(os.getenv("RESTART"))  # 是否用重启vps
-headless = bool(int(os.getenv("HEADLESS")))
-if headless:
-    print("headless mode")
-try:
-    filename = os.path.splitext(os.path.basename(__file__))[0]
-except Exception as e:
-    filename = "racknerd"
-cookieFileName = f'cookies.{filename}.pkl'
-#  PROXY = "127.0.0.1:1080"  #  HOST:PORT
-PROXY = "socks5://127.0.0.1:1081"  #  HOST:PORT
-chrome_options = webdriver.ChromeOptions()
-chrome_options.headless = headless
-if isProxy:
-    chrome_options.add_argument('--proxy-server=%s' % PROXY)
-chrome_options.add_argument("ignore-certificate-errors")
-chrome_options.add_argument("--window-size=1280,1080")
-chrome_options.add_argument("--lang=en-us")
-chrome_options.add_argument("--incognito")
-# 关闭提示条：”Chrome 正受到自动测试软件的控制”
-chrome_options.add_argument("--disable-infobars")
-# 不加载图片, 提升速度
-chrome_options.add_argument('blink-settings=imagesEnabled=false')
+        env_path = Path('.') / '.env'
+        load_dotenv(dotenv_path=env_path, verbose=True)
 
-driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://www.ipchicken.com/")
-#  driver.get("https://my.racknerd.com/index.php")
-driver.get("https://www.racknerd.com")
-if os.path.exists(cookieFileName):
-    #  url = 'https://justhost.ru'
-    cookies = pickle.load(open(cookieFileName, "rb"))
-    for cookie in cookies:
-        print(f"loading cookies:{cookie}")
-        driver.add_cookie(cookie)
-else:
-    pass
-
-url = "https://my.racknerd.com/index.php"
-driver.get(url)
-
-try:
-    driver.find_element_by_name('username').send_keys(
-        email)  # enter your email
-    driver.find_element_by_name('password').send_keys(
-        password)  # enter your password
-    #  print(f"passwd:{password}")
-    time.sleep(1.5)
-    loginbutton = driver.find_element_by_class_name('btn-primary')
-    if loginbutton:
-        loginbutton.click()
-    time.sleep(2)
-
-    if os.path.exists(cookieFileName):
-        # 重新登录系统，删除以前的cookie文件
-        os.remove(cookieFileName)
-        print("loading cookies")
-except Exception as e:
-    print("using cookies, no need login")
-    print(e.args)
-
-url = 'https://my.racknerd.com/clientarea.php'
-
-
-def checkurl(url):
-    counts = 100
-    print("waiting ", end='')
-    for i in range(counts):
-        time.sleep(2)
-        #  if driver.current_url != url:
-        if url in driver.current_url:
-            print(driver.current_url)
-            driver.switch_to.default_content()
-            break
-        print(".", end='', flush=True)
-
-
-checkurl(url)
-
-if not os.path.exists(cookieFileName):
-    # 保存cookies，下次免登录
-    pickle.dump(driver.get_cookies(), open(cookieFileName, "wb"))
-
-driver.find_element_by_id(
-    "ClientAreaHomePagePanels-Active_Products_Services-0").click()
-url = 'https://my.racknerd.com/clientarea.php?action=productdetails'
-checkurl(url)
-time.sleep(1)
-# scroll to end
-#  driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
-current_y = (driver.execute_script('return window.innerHeight') /
-             2) + driver.execute_script('return window.pageYOffset')
-scroll_y_by = current_y + 100
-driver.execute_script("window.scrollBy(0, arguments[0]);", scroll_y_by)
-
-#  reboot button
-#  driver.find_element_by_class_name("btn-default").click()
-#  print(driver.current_url)
-
-
-def restarting(isRestart):
-    if isRestart:
+        cls.email = os.getenv("EMAIL")
+        cls.password = os.getenv("PASSWORD")
+        cls.isProxy = int(os.getenv("PROXY"))  # 是否用proxy
+        cls.isRestart = int(os.getenv("RESTART"))  # 是否用重启vps
+        cls.headless = bool(int(os.getenv("HEADLESS")))
+        if cls.headless:
+            print("headless mode")
         try:
-            html = driver.find_element_by_id('main-body')
-            if html:
-                html.send_keys(Keys.PAGE_DOWN)
-            #  restart =driver.find_elements_by_link_text("Перезагрузите сервер")
-            restart = driver.find_elements_by_class_name("btn-default")
+            cls.filename = os.path.splitext(os.path.basename(__file__))[0]
         except Exception as e:
-            restart = driver.find_elements_by_class_name("btn-default")
-        if isinstance(restart, list):
-            for i in restart:
-                print(f"'{i.text}'")
-                if i.text.lower() == 'reboot':
-                    print(f"found '{i.text}'")
-                    i.click()
-                    time.sleep(1)
-                    delay = 10
-                    try:
-                        elem = WebDriverWait(driver, delay).until(
-                            EC.presence_of_element_located(
-                                (By.CLASS_NAME, 'modal-dialog')))
-                        #  elem=driver.find_elements_by_class_name("modal-dialog")
-                        if elem:
-                            elems = driver.find_elements_by_class_name(
-                                "btn-warning")
-                            print(f"reboot type:{type(elems)}")
-                            if isinstance(elems, list):
-                                for el in elems:
-                                    try:
-                                        print(f"modal element {el}")
-                                        print(f"{el.get_attribute('value')}")
-                                        el.send_keys("\n")
-                                        time.sleep(5)
-                                    except Exception as e:
-                                        pass
-                            else:
-                                #  elems.send_keys(Keys.ENTER)
-                                #  print(f"reboot type:{type(elems)}")
-                                elems = driver.find_elements_by_xpath(
-                                    '//*[@id="confirm-reboot"]/div/div/div[3]/input'
-                                )
-                                if elems:
-                                    elems.click()
+            cls.filename = "justhost"
+        cls.restarturl = "https://my.racknerd.com/clientarea.php"
 
-                    except TimeoutException:
-                        print("Loading took too much time!")
-                        print("not found modal-dialog")
+    def login(self, user="", password=""):
+        if len(password) == 0:
+            raise Exception("密码不对")
+        if len(user) == 0:
+            raise Exception("用户名不对")
+        # 根据登录界面修改下面变量
+        loginusername = "username"
+        loginid = "inputEmail"
+        loginpasswordid = "inputPassword"
 
-                    break
-        print("restarting!!")
+        self.assert_element(f'input[name="{loginusername}"]')
+        self._print("input username")
+        #  self.update_text(f"input#{loginid}", f"{user}\n")
+        self.type(f"#{loginid}", f"{user}")
+        self._print("input password")
+        #  self.update_text(f"input#{loginpasswordid}", f"{password}")
+        self.type(f"input#{loginpasswordid}", f"{password}")
+        self.wait_for_element_present("#login", timeout=20)
+        #  self.assert_text("Log in", "登录")
+        self.assert_element(f'input[id="{loginpasswordid}"]')
+        self.click("#login")
 
+    def restarting(self, isRestart=0):
+        if isRestart > 0:
+            try:
+                self.open(self.restarturl)
+                # logined
+                self.assert_text("Welcome Back")
+                self.wait_for_element_present('div[class="list-group"]', timeout=5)
+                # click Manage Product
+                #  self.click('div[class="list-group"]')
+                self._print(f"Active Products/Services")
+                self.click('//*[@id="ClientAreaHomePagePanels-Active_Products_Services-0"]')
+                self._print(f"Manage Product")
+                self.assert_text("Manage Product")
+                self._print(f"Ready to restart vps")
+                # reboot
+                self.wait_for_element_present('span[id="displayreboot"]', timeout=15)
+                self.click('//*[@id="displayreboot"]/a')
+                self.wait_for_element_present('div[class="modal-content"]', timeout=5)
+                # confirm
+                self.click('input[type="button"]')
+            except Exception as e:
+                self._print(e.args)
+                url="https://my.racknerd.com/clientarea.php?action=productdetails&amp;id=39227&amp;serveraction=custom&amp;a=reboot"
+                self.open(url)
+            self._print("restarting!!")
+        else:
+            self._print("no need restart!")
 
-def main():
-    try:
-        restarting(isRestart)
+    def test_basic(self):
+        url="https://my.racknerd.com/index.php"
+        url="https://my.racknerd.com"
+        url2=self.restarturl
+        try:
+            self.open(url)
+            #  self.asscert_text("VPS")
+            self.wait_for_element_present('a[href="#"]', timeout=5)
+        except Exception as e:
+            self._print("url timeout.")
+            time.sleep(3)
+        finally:
+            self.open(url2)
+            self._print(f"open {url2}")
+            time.sleep(1)
+            #  self.wait_for_element_present('a[href="/services/vps"]', timeout=10)
+
+        self.assert_element('a[href="#"]')
+        self._print("prepare login")
+        self.login(self.email, self.password)
+        #  time.sleep(1)
+        # click "Cloud Foundry apps"
+        try:
+            # active Services
+            css='div[class="panel-heading"]'
+            self.wait_for_element_present(css, timeout=8)
+            self.click(css)
+        except Exception as e:
+            self._print(e.args)
+            self.open(url2)
+        # click "..."
+        self._print(f"prepare to restart")
+        self.restarting(self.isRestart)
         time.sleep(5)
-    finally:
-        driver.close()
 
+    def login_to_swag_labs(self):
+        """ Login to Swag Labs and verify that login was successful. """
+        self.open("https://www.saucedemo.com")
+        self.type("#user-name", "standard_user")
+        self.type("#password", "secret_sauce")
+        self.click('input[type="submit"]')
+        self.open("https://my.racknerd.com/index.php?rp=/login")
+        self.type("#inputEmail", "123")
+        self.sleep(5)
 
-if __name__ == "__main__":
-    main()
+    def atest_A_LOGIN(self):
+        """ This test checks standard login for the Swag Labs store. """
+        self.login_to_swag_labs()
+        self.assert_element("#inventory_container")
+        self.assert_element('div:contains("Sauce Labs Backpack")')
--- python/selenium/racknerd/run.sh
+++ python/selenium/racknerd/run.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
-python racknerd.py &
+# python racknerd.py &
+
+# Activate Debug Mode on failures ("c" to continue)
+pytest --pdb -s --incognito --headed --proxy=socks5://127.0.0.1:1081 racknerd.py
+# pytest  --pdb -s --headed --proxy=socks5://127.0.0.1:1081 racknerd.py
+
+# nosetests --headed --proxy=socks5://127.0.0.1:1081 racknerd.py --pdb -s --incognito
+
 
