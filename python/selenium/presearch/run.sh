4c64acfc5c93b4cbf4f301df9961ebd36ea6454f
pchaos <drifthua@gmail.com>
2022-03-27 13:33:33 +0800

    update presearch

--- python/selenium/presearch/run.sh
+++ python/selenium/presearch/run.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 
 # python racknerd.py &
-
+proxy=socks5://127.0.0.1:1081
 # Activate Debug Mode on failures ("c" to continue)
-# pytest --pdb -s --incognito --headed --proxy=socks5://127.0.0.1:1081 racknerd.py
-# pytest --pdb -s --incognito --headed --proxy=socks5://127.0.0.1:1081 presearch.py
-# pytest -v --pdb -s --headed --proxy=socks5://127.0.0.1:1081 presearch.py
-# pytest --user-data-dir=${HOME}/.config/google-chrome --maximize -v --pdb -s --headed --proxy=socks5://127.0.0.1:1081 presearch.py
-pytest --user-data-dir="${HOME}/.config/google-chrome" --maximize -v --headed --proxy=socks5://127.0.0.1:1081 presearch.py
-# pytest  --pdb -s --headed --proxy=socks5://127.0.0.1:1081 racknerd.py
-
-# nosetests --headed --proxy=socks5://127.0.0.1:1081 racknerd.py --pdb -s --incognito
-
+# pytest --pdb -s --incognito --headed --proxy=${proxy} racknerd.py
+# pytest --pdb -s --incognito --headed --proxy=${proxy} presearch.py
+# pytest -v --pdb -s --headed --proxy=${proxy} presearch.py
+pytest --user-data-dir="${HOME}/.config/google-chrome" --maximize -v --pdb -s --headed --proxy=${proxy} presearch.py
+# pytest --user-data-dir="${HOME}/.config/google-chrome"  --maximize -v --headed --proxy=${proxy} presearch.py
+# pytest  --pdb -s --headed --proxy=${proxy} racknerd.py
