b78d74be8fd0c14f3ea2f747a1b5b09898cf9817
pchaos <drifthua@gmail.com>
2022-05-01 19:27:03 +0800

    update

--- python/selenium/justhost/run.sh
+++ python/selenium/justhost/run.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
+# conda install pytest
 # python justhost.selenium.py &
 
-# pytest  --headed --proxy=socks5://127.0.0.1:1081 justhost.py &
+# pytest  --headed --proxy=socks5://127.0.0.1:1081 justhost.py
 
 # Activate Debug Mode on failures ("c" to continue)
-pytest  --headed --proxy=socks5://127.0.0.1:1081 justhost.py --pdb -s --incognito
+pytest --pdb -s --incognito --headed --proxy=socks5://127.0.0.1:1081 justhost.py
