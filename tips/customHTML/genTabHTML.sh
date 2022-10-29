8b716d0dec391d711bfedca7d847da00b1658ee8
pchaos <drifthua@gmail.com>
2022-10-29 21:38:11 +0800

    update custom main

--- tips/customHTML/genTabHTML.sh
+++ tips/customHTML/genTabHTML.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
+# Modified: 2022-10-29 20:50:48
+
 pytest test_genTABHTML.py 
 echo copy files ... ~/myDocs/YUNIO/backup/main.htm 
-cp /tmp/main.htm /home/myuser/myDocs/YUNIO/backup/main.htm 
+cp /tmp/main.htm $HOME/myDocs/YUNIO/backup/main.htm 
 
 echo Done
 
--- tips/customHTML/test_genTABHTML.py
+++ tips/customHTML/test_genTABHTML.py
@@ -10,8 +10,10 @@
                    2019/9/9:
 -------------------------------------------------
 """
+import time
 import unittest
 from unittest import TestCase
+
 from .genTabHTML import genTABHTML
 
 
@@ -23,56 +25,52 @@ class TestGenTABHTML(TestCase):
         # inifile = '{}.ini'.format(flist[0])
         renderList = []
         for fn in flist:
-            inifile = '{}.ini'.format(fn)
+            inifile = "{}.ini".format(fn)
             gh = genTABHTML()
             # gh.outputFilename = fn
             gh.outputFilename = "test"
             gh.iniFilename = inifile
             try:
                 templateFile = "customHTML/template.tab.table.html"
-                of, render = gh.genHTML(None,
-                                        # of, render = gh.genHTML("a{}".format(fn),
-                                        title=fn.split(".")[0],
-                                        prettify=False,
-                                        template=templateFile)
+                of, render = gh.genHTML(
+                    None, title=fn.split(".")[0], prettify=False, template=templateFile
+                )
             except Exception as e:
                 templateFile = "template.tab.table.html"
-                of, render = gh.genHTML(None,
-                                        # of, render = gh.genHTML("a{}".format(fn),
-                                        title=fn.split(".")[0],
-                                        prettify=False,
-                                        template=templateFile)
+                print(f"{e} something error with {inifile}")
+                time.sleep(1)
+                of, render = gh.genHTML(
+                    None, title=fn.split(".")[0], prettify=False, template=templateFile
+                )
             print("输出文件完成 {}".format(of))
             # print(render)
             self.assertTrue(len(render) > 100)
             renderList.append(render)
         print(renderList)
         # main
-        inifile = '{}.ini'.format(flist[0])
+        inifile = "{}.ini".format(flist[0])
         gh = genTABHTML()
         # gh.outputFilename = fn
         gh.iniFilename = inifile
         try:
             templateFile = "template.tab.html"
-            render = gh.renders(renderList,
-                                prettify=True,
-                                # template="customHTML/template.tab.html",
-                                template=templateFile,
-                                title="Main")
+            render = gh.renders(
+                renderList, prettify=True, template=templateFile, title="Main"
+            )
         except Exception as e:
             templateFile = "customHTML/template.tab.html"
-            render = gh.renders(renderList,
-                                prettify=True,
-                                # template="customHTML/template.tab.html",
-                                template=templateFile,
-                                title="Main")
+            print(f"{e} something error with {inifile}")
+            time.sleep(1)
+            render = gh.renders(
+                renderList, prettify=True, template=templateFile, title="Main"
+            )
         saveText = ""
         for r in render:
             saveText += r
-        gh.save('main.htm', saveText)
+        gh.save("main.htm", saveText)
         print("输出文件完成 {}".format(render))
         print(f"{flist} saved in main.htm")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
