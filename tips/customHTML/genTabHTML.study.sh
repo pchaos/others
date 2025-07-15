c126895765c40c1725be936f75539f5a81079ba0
pchaos <drifthua@gmail.com>
2025-07-15 18:21:40 +0800

    update

--- tips/customHTML/genTabHTML.study.sh
+++ tips/customHTML/genTabHTML.study.sh
@@ -1,11 +1,28 @@
 #!/usr/bin/env bash
-# Modified: 2025-07-12 22:07:14
+# Modified: 2025-07-15 13:04:24
 
-# pytest test_genTABHTML.py
-# pytest --headless -vvv test_genTABHTML.py::TestGenPchaosGitIo
-# 使用 -s 参数来禁用标准输出捕获，print 的输出就会在测试运行时显示出来
-pytest -s --headless -vvv test_genTABHTML.py::TestGenPchaosGitIo
-# pytest --headless -vvv test_genTABHTML.py::TestGenPchaosGitIo::test_genHTML_phone
+# 是否启用详细模式的参数是 -v
+verbose_mode=false
+
+# 遍历所有传入的参数
+for arg in "$@"; do
+  if [[ "$arg" == "-v" ]]; then
+    verbose_mode=true
+    break
+  fi
+done
+
+# 根据是否启用详细模式来执行不同的语句
+if $verbose_mode; then
+  echo "Verbose mode activated"
+  # 使用 -s 参数来禁用标准输出捕获，print 的输出就会在测试运行时显示出来
+  pytest -s --headless -vvv test_genTABHTML.py::TestGenPchaosGitIo
+else
+  传入"-v"参数,显示更多内容
+  # pytest test_genTABHTML.py
+  pytest --headless -vvv test_genTABHTML.py::TestGenPchaosGitIo
+  # pytest --headless -vvv test_genTABHTML.py::TestGenPchaosGitIo::test_genHTML_phone
+fi
 echo copy files ... $HOME/myDocs/YUNIO/tmp/gupiao/pchaos.github.io/index.html index_phone.html
 cp -v --update script.js styles.tab.css styles.tab.phone.css $HOME/myDocs/YUNIO/tmp/gupiao/pchaos.github.io/
 cp -v /tmp/index.html /tmp/index_phone.html $HOME/myDocs/YUNIO/tmp/gupiao/pchaos.github.io/
