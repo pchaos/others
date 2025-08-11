d852e20e06b9bc76a0bdb8d94eaff9bc0ce17677
pchaos <drifthua@gmail.com>
2025-08-11 10:09:15 +0800

    update

--- tips/customHTML/genTabHTML.sh
+++ tips/customHTML/genTabHTML.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
-# Modified: 2024-11-16 23:38:04
+# Modified: 2025-08-06 12:45:44
 
 # pytest test_genTABHTML.py
 pytest --headless -vvv test_genTABHTML.py::TestGenTABHTML
+if [[ $? -ne 0 ]]; then
+  echo "生成文件失败, exiting."
+  exit 1
+fi
 echo copy files ... ~/myDocs/YUNIO/backup/main.htm
 cp -v --update /tmp/main.htm $HOME/myDocs/YUNIO/backup/main.htm
 cp -v --update /tmp/main.htm $HOME/Downloads/s1081.gfw.pac $HOME/myDocs/YUNIO/tmp/gupiao/pchaos.github.io/
